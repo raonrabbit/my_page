0d305c6c95c1efde16112cb052e3fe54eae126a9
raonrabbit <raonrabbit@gmail.com>
2025-10-22 12:00:00 +0900

    feat: implement main page layout and routing

--- my-page/src/app/page.tsx
+++ my-page/src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1 className="text-3xl font-bold text-shadow-lg text-shadow-amber-400">Hello</h1>;
+  return (
+    <div className="flex-col w-full justify-items-end font-light pt-2">
+      <div className="w-fit">
+        <h1 className="text-2xl font-light mb-4">Welcome</h1>
+        <div className="space-y-1">
+          <p>Born in 1999</p>
+          <p>In Korea, Seoul.</p>
+          <p>사람은 탐구하고 경험하는 것으로 성장할 수 있다고 믿습니다.</p>
+          <p>많은 사람들에게 좋은 경험을 제공하는 웹페이지를 개발하고 싶습니다.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
