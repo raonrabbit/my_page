ea90325f0f587946551049d6fc3795ac5e5be8dd
최준혁 <raonrabbit@gmail.com>
2025-10-23 20:29:50 +0900

    feat: remove physics in canvas

--- my-page/src/widgets/three-scene/ui/ThreeScene.tsx
+++ my-page/src/widgets/three-scene/ui/ThreeScene.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import * as THREE from "three";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { EffectComposer, Noise, ToneMapping, Vignette } from "@react-three/postprocessing";
-import { Sky } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  EffectComposer,
+  Noise,
+  ToneMapping,
+  Vignette,
+} from "@react-three/postprocessing";
+import { Html, Sky } from "@react-three/drei";
 import { usePathname } from "next/navigation";
-import { Physics } from "@react-three/rapier";
 import { AnimatedPebbleModel } from "./AnimatedPebbleModel";
 import { AnimatedPebble2Model } from "./AnimatedPebble2Model";
 
@@ -24,7 +28,6 @@ export default function ThreeScene() {
         }}
       >
         <ambientLight intensity={1} />
-        <Sky />
         <spotLight
           position={[-3, 8, 5]}
           angle={0.5}
@@ -37,47 +40,48 @@ export default function ThreeScene() {
           shadow-bias={-0.0001}
         />
         <Effects />
+        <Sky />
 
-        <Suspense fallback={null}>
-          <Physics gravity={[0, -9.81, 0]} debug>
-          <mesh position={[0.5, -1, -1]} rotation={[4.5, 0, 0]} receiveShadow>
-            <circleGeometry args={[4, 32]} />
-            <meshStandardMaterial transparent opacity={0.2} color="#FFFFFF" />
-          </mesh>
+        <mesh position={[0.5, -1, -1]} rotation={[4.5, 0, 0]} receiveShadow>
+          <circleGeometry args={[4, 32]} />
+          <meshStandardMaterial transparent opacity={0.2} color="#FFFFFF" />
+        </mesh>
 
-          <AnimatedPebbleModel
-            position={[0, 0, 0]}
-            scale={1.2}
-            castShadow
-            receiveShadow
-            visible={pathname === "/about/" || pathname === "/stacks/" || pathname === "/projects/"}
-            delay={0}
-            reverseDelay={600}
-          />
+        <AnimatedPebbleModel
+          position={[0, 0, 0]}
+          scale={1.2}
+          castShadow
+          receiveShadow
+          visible={
+            pathname === "/about/" ||
+            pathname === "/stacks/" ||
+            pathname === "/projects/"
+          }
+          delay={0}
+          reverseDelay={600}
+        />
 
-          <AnimatedPebble2Model
-            position={[0.5, 1, 0]}
-            scale={1}
-            rotation={[0, Math.PI / 2.2, 0]}
-            castShadow
-            receiveShadow
-            visible={pathname === "/stacks/" || pathname === "/projects/"}
-            delay={300}
-            reverseDelay={300}
-          />
+        <AnimatedPebble2Model
+          position={[0.5, 1, 0]}
+          scale={1}
+          rotation={[0, Math.PI / 2.2, 0]}
+          castShadow
+          receiveShadow
+          visible={pathname === "/stacks/" || pathname === "/projects/"}
+          delay={300}
+          reverseDelay={300}
+        />
 
-          <AnimatedPebbleModel
-            position={[0.5, 2.1, 0]}
-            scale={1}
-            rotation={[Math.PI, Math.PI * 1.3, 0]}
-            castShadow
-            receiveShadow
-            visible={pathname === "/projects/"}
-            delay={600}
-            reverseDelay={0}
-          />
-          </Physics>
-        </Suspense>
+        <AnimatedPebbleModel
+          position={[0.5, 2.1, 0]}
+          scale={1}
+          rotation={[Math.PI, Math.PI * 1.3, 0]}
+          castShadow
+          receiveShadow
+          visible={pathname === "/projects/"}
+          delay={600}
+          reverseDelay={0}
+        />
       </Canvas>
     </div>
   );
@@ -88,7 +92,6 @@ function Effects() {
   const look = new THREE.Vector3();
 
   useFrame(({ camera, pointer }, dt) => {
-
     target.set(pointer.x - 1, 5 + pointer.y, 15 + Math.atan(pointer.x * 2));
 
     const k = 3;
@@ -101,7 +104,12 @@ function Effects() {
   });
 
   return (
-    <EffectComposer stencilBuffer enableNormalPass={false} autoClear={false} multisampling={4}>
+    <EffectComposer
+      stencilBuffer
+      enableNormalPass={false}
+      autoClear={false}
+      multisampling={4}
+    >
       <ToneMapping />
       <Noise premultiply opacity={0.4} />
       <Vignette eskil={false} offset={0.1} darkness={0.6} />
